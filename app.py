ee020e77e99cfa4ad132e3c5b04c1dc1548a31f3
dineshpattem <38723490+dineshpattem@users.noreply.github.com>
2018-05-09 13:07:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,7 @@ def makeWebhookResult(data):
     # print(json.dumps(item, indent=4))
     
     speech =  "Weather Forecast in " + location.get('city') + ": " + condition.get('text') + \
-             ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + " for " + condition.get('date') + " Forecast details: "  
+             ", the temperature is " + condition.get('temp') + " " + units.get('temperature') + " for \n " + condition.get('date') 
 
                 
     print("Response:")
